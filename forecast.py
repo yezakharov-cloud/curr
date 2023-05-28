152b0ea702b5c020707415ad3391371744e8b310
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2023-05-28 20:47:02 +0300

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,87 +1,63 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
-from tensorflow import keras
 import tensorflow as tf
+import torch
 
-# Load the CSV file and preprocess the data
+# Function to load historical exchange rate data from CSV file
 def load_data(file_path):
-    df = pd.read_csv(file_path)
-    df['Date'] = pd.to_datetime(df['Date'])
-    df.sort_values('Date', inplace=True)
-    return df
+    data = pd.read_csv(file_path)
+    return data
 
-# Train a neural network using TensorFlow
-def train_tensorflow_model(df):
-    # Perform TensorFlow model training here
-    # Replace this with your actual TensorFlow training code
-    # For demonstration purposes, we'll just print the summary of a dummy model
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(10, input_shape=(1,)),
-        tf.keras.layers.Dense(1)
-    ])
-    model.compile(optimizer='adam', loss='mse')
-    model.summary()
+# Function to train a neural network using Tensorflow
+def train_tensorflow_network(data):
+    # Implement your Tensorflow training code here
+    st.write("Training Tensorflow neural network...")
 
-# Train a neural network using Keras
-def train_keras_model(df):
-    # Perform Keras model training here
-    # Replace this with your actual Keras training code
-    # For demonstration purposes, we'll just print the summary of a dummy model
-    model = keras.models.Sequential([
-        keras.layers.Dense(10, input_shape=(1,)),
-        keras.layers.Dense(1)
-    ])
-    model.compile(optimizer='adam', loss='mse')
-    model.summary()
+# Function to train a neural network using PyTorch
+def train_pytorch_network(data):
+    # Implement your PyTorch training code here
+    st.write("Training PyTorch neural network...")
 
-# Predict exchange rate using TensorFlow model
-def predict_tensorflow_rate(df, date):
-    # Perform TensorFlow prediction here
-    # Replace this with your actual TensorFlow prediction code
-    # For demonstration purposes, we'll just return a random value
-    return 1.23
+# Function to predict exchange rate using Tensorflow
+def predict_tensorflow_rate(date):
+    # Implement your Tensorflow prediction code here
+    st.write("Predicting exchange rate using Tensorflow...")
 
-# Predict exchange rate using Keras model
-def predict_keras_rate(df, date):
-    # Perform Keras prediction here
-    # Replace this with your actual Keras prediction code
-    # For demonstration purposes, we'll just return a random value
-    return 1.34
+# Function to predict exchange rate using PyTorch
+def predict_pytorch_rate(date):
+    # Implement your PyTorch prediction code here
+    st.write("Predicting exchange rate using PyTorch...")
 
 # Main function
 def main():
-    st.title("Exchange Rate Prediction App")
-    
-    # CSV file selection window
-    uploaded_file = st.file_uploader("Upload CSV file", type="csv")
-    
-    if uploaded_file is not None:
-        df = load_data(uploaded_file)
-        
-        # Train a neural network based on the Tensorflow library
-        if st.button("Train a neural network based on the Tensorflow library"):
-            train_tensorflow_model(df)
-            st.write("TensorFlow model training completed.")
-        
-        # Train a neural network based on the Keras library
-        if st.button("Train a neural network based on the Keras library"):
-            train_keras_model(df)
-            st.write("Keras model training completed.")
-        
-        # Date selection field
-        selected_date = st.date_input("Select a date for exchange rate prediction")
-        
-        # Currency rate prediction based on the Tensorflow library
-        if st.button("Currency rate prediction based on the Tensorflow library"):
-            rate = predict_tensorflow_rate(df, selected_date)
-            st.write("Predicted exchange rate (TensorFlow):", rate)
-        
-        # Currency rate prediction based on the Keras library
-        if st.button("Currency rate prediction based on the Keras library"):
-            rate = predict_keras_rate(df, selected_date)
-            st.write("Predicted exchange rate (Keras):", rate)
+    st.title("Exchange Rate Prediction")
 
-if __name__ == '__main__':
+    # CSV file upload
+    st.header("Upload Historical Data (CSV)")
+    file = st.file_uploader("Upload CSV", type="csv")
+    if file is not None:
+        data = load_data(file.name)
+        st.success("File uploaded successfully!")
+
+    # Train Tensorflow network button
+    if st.button("Train a neural network based on the Tensorflow library"):
+        train_tensorflow_network(data)
+
+    # Train PyTorch network button
+    if st.button("Train a neural network based on the Pytorch library"):
+        train_pytorch_network(data)
+
+    # Date selection for prediction
+    st.header("Select Date for Exchange Rate Prediction")
+    selected_date = st.date_input("Select a date")
+
+    # Predict exchange rate using Tensorflow button
+    if st.button("Currency rate prediction based on the Tensorflow library"):
+        predict_tensorflow_rate(selected_date)
+
+    # Predict exchange rate using PyTorch button
+    if st.button("Currency rate prediction based on the Pytorch library"):
+        predict_pytorch_rate(selected_date)
+
+if __name__ == "__main__":
     main()
-
