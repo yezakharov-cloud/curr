8b83ae7d46aa3bc282e5a040cadd3de5c62f0aa0
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2023-05-29 11:57:26 +0300

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -3,12 +3,10 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import streamlit as st
-import theano
-import theano.tensor as T
-from theano import function
-from theano import shared
-from theano.tensor.nnet import sigmoid
-from theano.tensor import tanh
+import mxnet as mx
+from mxnet import gluon, autograd, ndarray
+from mxnet.gluon import nn, Trainer
+from mxnet.gluon.loss import L2Loss
 
 # Load the historical exchange rate data from a CSV file
 def load_data(file_path):
@@ -20,80 +18,80 @@ def preprocess_data(data):
     # Convert dates to numerical values
     data['Date'] = pd.to_datetime(data['Date'])
     data['Date'] = data['Date'].map(pd.Timestamp.to_julian_date)
-    
+
     # Scale the exchange rate values to a range between 0 and 1
     scaler = MinMaxScaler()
     data['Rate'] = scaler.fit_transform(data['Rate'].values.reshape(-1, 1))
-    
+
     return data
 
-# Build the neural network model using Theano
+# Build the neural network model using MXNet
 def build_model(input_dim):
-    W_xh = shared(np.random.randn(input_dim, 64) * 0.01)
-    W_hh = shared(np.random.randn(64, 64) * 0.01)
-    W_hy = shared(np.random.randn(64, 1) * 0.01)
-    b_h = shared(np.zeros(64))
-    b_y = shared(0.)
-    
-    X = T.matrix()
-    y = T.vector()
-    
-    def step(x_t, h_tm1):
-        h_t = sigmoid(T.dot(x_t, W_xh) + T.dot(h_tm1, W_hh) + b_h)
-        y_t = T.dot(h_t, W_hy) + b_y
-        return h_t, y_t
-    
-    [h, y_pred], _ = theano.scan(
-        step,
-        sequences=X,
-        outputs_info=[dict(initial=T.zeros(64)), None]
-    )
-    
-    cost = T.mean((y - y_pred.flatten())**2)
-    gradients = T.grad(cost, [W_xh, W_hh, W_hy, b_h, b_y])
-    
-    learning_rate = 0.1
-    updates = [
-        (param, param - learning_rate * gradient)
-        for param, gradient in zip([W_xh, W_hh, W_hy, b_h, b_y], gradients)
-    ]
-    
-    train_model = function(
-        inputs=[X, y],
-        outputs=cost,
-        updates=updates
-    )
-    
-    predict_model = function(
-        inputs=[X],
-        outputs=y_pred.flatten()
-    )
-    
-    return train_model, predict_model
+    net = nn.Sequential()
+    with net.name_scope():
+        net.add(nn.Dense(64, activation='relu'))
+        net.add(nn.Dense(1))
+    return net
 
 # Train the model
-def train_model(train_fn, X_train, y_train):
+def train_model(model, X_train, y_train):
+    loss = L2Loss()
+    trainer = Trainer(model.collect_params(), 'adam')
     num_epochs = 50
+    batch_size = 32
+    train_data = gluon.data.DataLoader(gluon.data.ArrayDataset(X_train, y_train), batch_size=batch_size, shuffle=True)
     for epoch in range(num_epochs):
-        cost = train_fn(X_train, y_train)
+        cumulative_loss = 0.0
+        for X, y in train_data:
+            with autograd.record():
+                output = model(X)
+                l = loss(output, y)
+            l.backward()
+            trainer.step(batch_size)
+            cumulative_loss += ndarray.mean(l).asscalar()
         if epoch % 10 == 0:
-            print("Epoch {}: Cost = {}".format(epoch, cost))
+            print("Epoch {}: Loss = {}".format(epoch, cumulative_loss / len(train_data)))
 
 # Make predictions using the trained model
-def predict(predict_fn, X_test):
-    return predict_fn(X_test)
+def predict(model, X_test):
+    return model(X_test).asnumpy().flatten()
 
 # Main function
 def main():
     # Set Streamlit app title and layout
     st.title("Exchange Rate Prediction")
     st.sidebar.title("Options")
-    
+
     # Load and preprocess the data
     file_path = st.sidebar.file_uploader("Upload CSV file", type="csv")
-    
+
     if file_path is not None:
         data = load_data(file_path)
         data = preprocess_data(data)
-        
+
+        # Split the data into input features (X) and target variable (y)
+        X = data[['Number', 'Date']].values
+        y = data['Rate'].values
+
+        # Split the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Convert the data to MXNet NDArray format
+        X_train = ndarray.array(X_train)
+        y_train = ndarray.array(y_train)
+
+        # Build the neural network model
+        input_dim = X_train.shape[1]
+        model = build_model(input_dim)
+        model.initialize(mx.init.Xavier())
+
+        # Train the model
+        train_model(model, X_train, y_train)
+
+        # Convert the test data to MXNet NDArray format
+        X_test = ndarray.array(X_test)
+
+        # Make predictions
+        predictions = predict(model, X_test)
+
         #
