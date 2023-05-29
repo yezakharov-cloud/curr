efb545ba0008b93081f8d36a44de26dd57a87c59
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2023-05-29 13:16:33 +0300

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,84 +1,68 @@
 import streamlit as st
-import torch
-import torch.nn as nn
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from sklearn.preprocessing import MinMaxScaler
-
 
 # Load historical exchange rate data from CSV file
 def load_data(filename):
     data = pd.read_csv(filename)
     return data
 
-
-# Define the neural network model using PyTorch
-class ExchangeRatePredictor(nn.Module):
-    def __init__(self):
-        super(ExchangeRatePredictor, self).__init__()
-        self.fc1 = nn.Linear(1, 10)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(10, 1)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        return x
-
-
-# Preprocess the data for training the neural network
-def preprocess_data(data):
-    # Normalize the data
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    data['Rate'] = scaler.fit_transform(data['Rate'].values.reshape(-1, 1))
-    return data, scaler
-
-
-# Train the neural network
-def train_model(data):
-    model = ExchangeRatePredictor()
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-    x = torch.Tensor(data['Number'].values).unsqueeze(1)
-    y = torch.Tensor(data['Rate'].values).unsqueeze(1)
-
-    for epoch in range(1000):
-        optimizer.zero_grad()
-        outputs = model(x)
-        loss = criterion(outputs, y)
-        loss.backward()
-        optimizer.step()
-
+# Create a Keras model for exchange rate prediction
+def create_model():
+    model = Sequential()
+    model.add(Dense(units=50, activation='relu', input_shape=(1,)))
+    model.add(Dense(units=50, activation='relu'))
+    model.add(Dense(units=1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
     return model
 
+# Train the Keras model
+def train_model(model, data):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data)
+
+    x_train = scaled_data[:-1]
+    y_train = scaled_data[1:]
+
+    model.fit(x_train, y_train, epochs=20, batch_size=1, verbose=2)
+
 # Predict the exchange rate using the trained model
-def predict_rate(model, rate, scaler):
-    x = torch.Tensor([rate])
-    x = scaler.transform(x.reshape(-1, 1))
-    x = torch.Tensor(x)
-    prediction = model(x)
-    prediction = scaler.inverse_transform(prediction.detach().numpy().reshape(-1, 1))
-    return prediction.item()
+def predict_rate(model, data):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data)
 
+    x_test = scaled_data[-1]
+    predicted_rate = model.predict(np.array([x_test]))
+    predicted_rate = scaler.inverse_transform(predicted_rate)
+    return predicted_rate[0][0]
 
-
-
-# Streamlit application
+# Main function
 def main():
     st.title('Exchange Rate Prediction')
-    st.write('Upload the historical exchange rate data file (CSV format):')
-    file = st.file_uploader('Upload CSV file', type=['csv'])
 
-    if file is not None:
-        data = load_data(file)
-        data, scaler = preprocess_data(data)
-        model = train_model(data)
-        last_rate = data['Rate'].values[-1]
-        prediction = predict_rate(model, last_rate, scaler)
-        st.write(f'The predicted exchange rate for the next time step is: {prediction}')
+    # Upload historical data file
+    uploaded_file = st.file_uploader('Upload CSV file', type=['csv'])
 
+    if uploaded_file is not None:
+        data = load_data(uploaded_file)
 
+        st.subheader('Historical Data')
+        st.dataframe(data)  # Display all loaded values
+
+        model = create_model()
+
+        st.subheader('Train Model')
+        train_model(model, data['Rate'].values.reshape(-1, 1))
+        st.write('Model training complete.')
+
+        st.subheader('Exchange Rate Prediction')
+        prediction = predict_rate(model, data['Rate'].values.reshape(-1, 1))
+        st.write('Predicted exchange rate:', prediction)
+
+# Run the application
 if __name__ == '__main__':
     main()
