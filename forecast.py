f4d71fa0c3c1af6984b567ac769fd789001e63ad
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2023-06-02 21:29:05 +0300

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -6,6 +6,26 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dense, LSTM
 from sklearn.preprocessing import MinMaxScaler
+from flask import Flask, request, render_template_string
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form['rate']
+        current_rate = f'{user_input:.6f}'  # Adds six zeros after the decimal point
+        return f"The current rate is: {current_rate}"
+    return render_template_string('''
+        <form method="POST" action="/">
+            <label for="rate">Enter the rate:</label>
+            <input type="number" step="0.000001" name="rate" id="rate" required>
+            <input type="submit" value="Submit">
+        </form>
+    ''')
+
+
 
 
 # Load historical exchange rate data from CSV file
