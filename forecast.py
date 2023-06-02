8521171c5c2d1b0c8174bbc79a1e5f79010b998f
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2023-06-02 21:40:09 +0300

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,18 +1,13 @@
 import streamlit as st
 
-def add_zeros(digits):
-    current_rate = '.'.join(digits) + '000000'
-    return current_rate
+def add_zeros(current_rate):
+    return f'{current_rate:.6f}'
 
 def main():
     st.title("Rate Calculator")
-    digits = []
-    for i in range(8):
-        digit = st.text_input(f"Digit {i+1}", max_chars=1)
-        digits.append(digit)
-
-    if all(digits):
-        result = add_zeros(digits)
+    current_rate = st.text_input("Enter the rate (e.g., 12.345678):")
+    if current_rate:
+        result = add_zeros(float(current_rate))
         st.write(f"The current rate is: {result}")
 
 if __name__ == '__main__':
