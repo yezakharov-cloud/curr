9e906225f971351eef078626a15bc8fba15fd714
yezakharov-cloud <119701833+yezakharov-cloud@users.noreply.github.com>
2023-06-02 19:30:55 +0300

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dense, LSTM
 from sklearn.preprocessing import MinMaxScaler
 
